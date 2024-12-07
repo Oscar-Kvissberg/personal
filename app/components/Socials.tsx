49910559e11de9df5aaf7ec5221a31fa46e07277
Oscar-Kvissberg <oscarkvissberg@gmail.com>
2024-12-07 03:00:36 +0100

    d
    d

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -6,17 +6,17 @@ export default function Socials() {
     return (
         <>
             <LinkWithIcon
-                href="https://linkedin.com"
+                href="https://www.linkedin.com/in/oscar-kvissberg/"
                 text="LinkedIn"
                 icon={<FaLinkedin className="w-5 h-5" />}
             />
             <LinkWithIcon
-                href="https://github.com"
+                href="https://github.com/Oscar-Kvissberg"
                 text="GitHub"
                 icon={<FaGithub className="w-5 h-5" />}
             />
             <LinkWithIcon
-                href="mailto:example@email.com"
+                href="mailto:oscarkvissberg@gmail.com"
                 text="Email"
                 icon={<HiOutlineMail className="w-5 h-5" />}
             />
