dd60fc9d97fa1c35421ed1857bf023ed4ce22161
Oscar-Kvissberg <oscarkvissberg@gmail.com>
2024-12-10 00:34:28 +0100

    d
    d

--- app/components/ui/Tabs.tsx
+++ app/components/ui/Tabs.tsx
@@ -13,7 +13,7 @@ const TabsList = React.forwardRef<
     <TabsPrimitive.List
         ref={ref}
         className={cn(
-            "inline-flex h-9 items-center justify-center rounded-lg bg-white/10 p-1 text-white/60 border border-white/20",
+            "inline-flex items-center h-10 justify-center rounded-lg bg-white/10 p-1 text-white/60 border border-white/20",
             className
         )}
         {...props}
@@ -28,7 +28,11 @@ const TabsTrigger = React.forwardRef<
     <TabsPrimitive.Trigger
         ref={ref}
         className={cn(
-            "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-white/10 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/20 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white/20 data-[state=active]:text-white data-[state=active]:shadow",
+            "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-white/10 transition-all",
+            "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/20 focus-visible:ring-offset-2",
+            "disabled:pointer-events-none disabled:opacity-50",
+            "data-[state=active]:bg-white/20 data-[state=active]:text-white data-[state=active]:shadow",
+            "data-[state=inactive]:hover:bg-white/10 data-[state=inactive]:hover:text-white",
             className
         )}
         {...props}
